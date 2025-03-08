14cbde77f2619a22006eace2e209cd808abfe848
RayanKabra <rkabra.dev@gmail.com>
2025-03-08 14:24:12 +0100

    Calling routes before bdd connection

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Import de mes routes
+const userRoutes = require("./routes/user");
+
+// utilisation de mes routes
+app.use(userRoutes);
+
 // Initialisation .env
 require("dotenv").config();
 
 // Connexion DB Mongo
 mongoose.connect(process.env.MONGODB_URI);
-
-// Mmport de mes routes
-const userRoutes = require("./routes/user");
-
-// utilisation de mes routes
-app.use(userRoutes);
 
 app.all("*", (req, res) => {
   res.status(404).json({ error: "Cette route n'existe pas" });
